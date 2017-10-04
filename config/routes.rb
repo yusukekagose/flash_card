d44036c2c4b492f93bf8be0eea0a466d48b2047a
kagose yusuke <>
2017-10-04 11:31:33 +0900

    create User controller and show and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/index'
+
   resources :words
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   root 'home#index'
