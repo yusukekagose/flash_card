dcbc70675dc6aa3e9891e3d4a1586fcc4f90e101
kagose yusuke <>
2017-10-09 18:53:42 +0900

    amend layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   access all: [:index], user: {except: [:destroy,:new,:create, :update, :edit, :toggle_status]}, site_admin: :all
   def index
+    @meanings = Meaning.all
   end
 end
