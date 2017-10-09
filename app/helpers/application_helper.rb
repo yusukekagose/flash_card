ff0eca2fe52c16662bc4b3d3fb6c9355e75359e5
kagose yusuke <>
2017-10-09 17:16:40 +0900

    side bar each do widtindex

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,8 +34,33 @@ module ApplicationHelper
 
   end
 
+  def parts
+    [1,2,3,4,5]
+  end
 
+  def levels
+    ["初級","中級","上級"]
+  end
 
+  def side_bar_helper
+    sidebars = ''
+    i = 1
+
+    levels.each do |level|
+      sidebars << "<div class='collapse text-center' id='menu#{i.to_s}'>
+        <a href='#menu#{i.to_s}sub#{i.to_s}' class='list-group-item list-group-item-action' data-toggle='collapse' aria-expanded='false'> #{level}</a>"
+        parts.each do |part|
+        "<div class='collapse' id='menu#{i.to_s}sub#{i.to_s}'>
+            <a href='#' class='list-group-item list-group-item-action' data-parent='menu#{i.to_s}'>#{part}</a>
+        </div>"
+        end
+        "</div>"
+        i += 1
+    end
+
+    sidebars.html_safe
+
+  end
 
 
 end
