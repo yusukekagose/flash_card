c8b1696ac5238d860f5bca0c75fb453448954373
kagose yusuke <>
2017-11-07 11:06:36 +0900

    form and search funtion for meaning

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   access all: [:index], user: {except: [:destroy,:new,:create, :update, :edit, :toggle_status]}, site_admin: :all
   def index
     @meanings = Meaning.all
+    @meanings = @meanings.eager_load(:word).where( word: {english_word: params["english_word"]}) if params["english_word"].present?
+    @meanings = @meanings.where(meaning: params["meaning"]) if params["meaning"].present?
     @dictionary = Dictionary.new
     @like = Like.new
   end
